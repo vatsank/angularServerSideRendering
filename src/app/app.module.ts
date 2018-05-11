0583fc5e67a75065bbbb4461c4927baf46940858
Srivatsan <vatsank@gmail.com>
2018-05-11 09:37:45 +0530

    Server Side Rendering with Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule }    from '@angular/common/http';
 
+import { AppRoutingModule }     from './app-routing.module';
+
+import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { ShowMoviesComponent } from './show-movies/show-movies.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule.withServerTransition({ appId: 'tour-of-heroes' }),
+    ],
+  declarations:[AppComponent, ShowMoviesComponent],
+  providers: [  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string) {
+    const platform = isPlatformBrowser(platformId) ?
+      'in the browser' : 'on the server';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
---
+++ src/app/show-movies/show-movies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-movies',
+  templateUrl: './show-movies.component.html',
+  styleUrls: ['./show-movies.component.css']
+})
+export class ShowMoviesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
